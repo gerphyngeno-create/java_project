eac1ae340d1728eb5396b23474e76ae8bbfec32a
gerphyngeno <gerphy.ngeno@strathmore.edu>
2026-02-25 09:59:36 +0300

    edit any error

---
+++ src/University System.java/Person.java
@@ -0,0 +1,21 @@
+package University System.java;
+
+public class Person {
+    private String name;
+    private int id;
+
+    public Person (String name, int id){
+        this.name = name;
+        this.id = id;
+
+    }
+    public String getname(){
+        return name;
+    }
+    public int getid(){
+        return id;
+    } 
+    public abstract void displayrole();
+    
+    
+}

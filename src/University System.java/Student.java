c0cf553b15211514645524c48c384aace3339690
gerphyngeno <gerphy.ngeno@strathmore.edu>
2026-02-25 22:27:48 +0300

    Lecturer class

---
+++ src/University System.java/Student.java
@@ -0,0 +1,33 @@
+package University system.java;
+
+public class Student {
+    public class Student extends Person implements Enrollable, Payable {
+
+    private String program;
+
+    public Student(String name, int id, String program) {
+        super(name, id);
+        this.program = program;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    @Override
+    public void enrollCourse(String courseName) {
+        System.out.println(getName() + " enrolled in " + courseName);
+    }
+
+    @Override
+    public void payFee(double amount) {
+        System.out.println(getName() + " paid fee: " + amount);
+    }
+
+    @Override
+    public void displayRole() {
+        System.out.println("I am a Student.");
+    }
+}
+    
+}
